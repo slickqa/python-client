75e8c711ad01a8530981c81f11eec768b32702e0
Chris Saxey <csaxey@accessdata.com>
2013-10-02 15:34:18 -0600

    added some comments to give a better example of how to use the high level api

--- testcon.py
+++ testcon.py
@@ -1,12 +1,31 @@
 from slickqa import SlickQA
-from slickqa import SlickConnection, ResultStatus, Configuration, Project
+from slickqa import SlickConnection, ResultStatus, Configuration
 from slickqa.queries import ConfigurationQuery
 
 url = 'http://localhost:8080'
 
+# sample of how to use the lower level api
 slick = SlickConnection(url)
 all_projects = slick.projects.find()
 print(("Here are all the projects: {}".format(all_projects)))
+allconfigs = slick.configurations.find()
+print("All Configurations (there are " + str(len(allconfigs)) + " of them):")
+for config in allconfigs:
+    assert(isinstance(config, Configuration))
+    print("\t Name: " + config.name + ", type: " + config.configurationType + ", id: " + config.id)
+
+configs = slick.configurations.find(ConfigurationQuery(configurationType="ENVIRONMENT"))
+
+print("\nThere are " + str(len(configs)) + " ENVIRONMENT configurations in slick:\n")
+for config in configs:
+    assert(isinstance(config, Configuration))
+    print("\t Name: " + config.name + ", type: " + config.configurationType + ", filename: " + config.filename + ", id: " + config.id)
+
+proj = slick.projects.findByName("Slickij Developer Project")
+print("\nFound Project '%s' with %d components." % (proj.name, len(proj.components)))
+
+# sample of how to use the higher level api
+# this is good if you just need to shove the results up to slick and don't need to do anything tricky
 high = SlickQA(url, "test1", "1.0", "311", "Smoke")
 if high.is_connected:
     high.add_log_entry("One message")
@@ -24,19 +43,3 @@ if high.is_connected:
     high.add_log_entry("3")
     high.add_log_entry("4")
     high.file_result("tc4", ResultStatus.BROKEN_TEST, "THERE ARE FOUR LIGHTS!!!", 4)
-
-allconfigs = slick.configurations.find()
-print("All Configurations (there are " + str(len(allconfigs)) + " of them):")
-for config in allconfigs:
-    assert(isinstance(config, Configuration))
-    print("\t Name: " + config.name + ", type: " + config.configurationType + ", id: " + config.id)
-
-configs = slick.configurations.find(ConfigurationQuery(configurationType="ENVIRONMENT"))
-
-print("\nThere are " + str(len(configs)) + " ENVIRONMENT configurations in slick:\n")
-for config in configs:
-    assert(isinstance(config, Configuration))
-    print("\t Name: " + config.name + ", type: " + config.configurationType + ", filename: " + config.filename + ", id: " + config.id)
-
-proj = slick.projects.findByName("Slickij Developer Project")
-print("\nFound Project '%s' with %d components." % (proj.name, len(proj.components)))
