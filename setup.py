617c2a643124ddff78e10e151630ceac05fac1ab
Jason Corbett <jason.corbett@vivint.com>
2015-04-21 12:44:48 -0600

    updating readme and setup

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     description="A client library for the Slick QA result database",
     version="2.0" + open("build.txt").read(),
     license="License :: OSI Approved :: Apache Software License",
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst', '*.html']},
     include_package_data=True,
-    install_requires=['requests>=1.1.0',],
+    install_requires=['requests>=1.1.0', ],
     author="Slick Developers",
-    url="http://code.google.com/p/slickqa"
+    url="http://github.com/slickqa/python-client"
 )
